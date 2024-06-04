55f58f1bae1cbf05f4506eafceeb949b55bb2fbf
Martin Unger <martin-unger2@live.at>
2024-06-04 13:02:10 +0200

    register admin message + chat, model for Chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Chat, Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat','text', 'created_at', 'author', 'receiver')
+    list_display = ('text', 'created_at', 'author', 'receiver')
+    search_fields = ('text',)
+    
+
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
